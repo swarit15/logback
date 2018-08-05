69317ebb7ca9aafeaea04b845b7412e9e2bd9ff0
Swarit <swarit.it@gmail.com>
2018-08-05 20:10:11 +0530

    log poc

---
+++ logback-poc/src/main/java/com/logback/LogbackPocApplication.java
@@ -0,0 +1,13 @@
+package com.logback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogbackPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogbackPocApplication.class, args);
+		
+	}
+}
